8484bb62808d7915ac5d23db5fc4dc3819c7e6a7
Sean Omlor <somlor@eml.cc>
2014-07-03 21:38:48 -0400

    add error handling for bad operators to calc

--- js/src/calc.js
+++ js/src/calc.js
@@ -1,29 +1,49 @@
 $(function() {
+  // return value pulled out of event target
   var inputVal = function(event) {
     return $(event.currentTarget).val();
   }
 
+  // return numeric value or blank string;
+  var numericOrBlank = function(value) {
+    return $.isNumeric(value) ? value : '';
+  }
+
+  // true true/false if operator is valid
   var validOperator = function(operator) {
     return ['+', '-', '*', '/', '%'].indexOf(operator) > -1;
   }
 
-  var operate = function(a, b, operator) {
-    switch(operator) {
-      case '+': return a + b;
-      case'-': return a - b;
-      case '*': return a * b;
-      case '/': return a / b;
-      case '%': return a % b;
-      default: return a + b;
+  // push operator event and any errors into operator event stream
+  var validateOperator = function(event) {
+    // always push operator
+    this.push(event);
+
+    // push error if operator is invalid
+    if (!validOperator(event.value())) {
+      this.push(new Bacon.Error('Invalid operator'));
     }
   }
 
+  // return operation applied to a and b values
+  var operate = function(a, b, operator) {
+    switch(operator) {
+      case '+': return a + b;
+      case '-': return a - b;
+      case '*': return a * b;
+      case '/': return a / b;
+      case '%': return a % b;
+      default: return NaN;
+    }
+  }
+
+  // jquery inputs
   var $inputA = $('input[name="a"]');
   var $inputB = $('input[name="b"]');
   var $selectOperator = $('select[name="operator"]');
   var $inputAnswer = $('input[name="answer"]');
 
-  // on keyup parseFloat values, keep numbers, convert to property
+  // on keyup parse value to float, keep if number, convert to property
   var a = $inputA.asEventStream('keyup')
                  .map(inputVal)
                  .map(parseFloat)
@@ -36,11 +56,22 @@ $(function() {
                  .filter($.isNumeric)
                  .toProperty(0);
 
+  // on change validate and convert to property
   var operator = $selectOperator.asEventStream('change')
                                 .map(inputVal)
-                                .filter(validOperator)
-                                .toProperty('+');
+                                .withHandler(validateOperator)
+                                .toProperty($selectOperator.val());
 
+  // combine a, b and operator properties via 'operate' function
   var answer = Bacon.combineWith(operate, a, b, operator);
-  answer.assign($inputAnswer, 'val');
+
+  // update answer field
+  answer.onValue(function(value) {
+    var value = numericOrBlank(value);
+    $inputAnswer.val(value);
+  });
+
+  // add/remove error class from operator select
+  operator.onValue(function() { $selectOperator.removeClass('error'); })
+  operator.onError(function() { $selectOperator.addClass('error'); })
 });
